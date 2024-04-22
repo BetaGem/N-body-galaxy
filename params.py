58d577ce474e08639a280e0727dae9fd2c053d17
Qifeng Huang <qfhuang0616@outlook.com>
2024-04-22 19:42:10 +0800

    Update params.py

--- params.py
+++ params.py
@@ -1,7 +1,7 @@
-N  =  15849 # number of particles
+N  =  4096  # number of particles
 Ng =  512   # number of grids, with size h = 1
 dt =  0.1   # time step
-t_tot = 16  # total time
+t_tot = 8   # total time
 
 G = 1e2       # gravitational const.
 
